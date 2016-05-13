cb5c8d9c5b2f8d473eb35c87e672de6833c83c74
Lluis Ingles <lluis@trovit.com>
2016-05-13 16:45:54 +0200

    Add tagsinfo, tagsbt and glolbt alias

--- git-alias.plugin.zsh
+++ git-alias.plugin.zsh
@@ -1,2 +1,23 @@
+tagsBetween() {
+	if [ -z "$1" ] && [ -z "$2" ]
+		then
+		echo "Tags between dates: since and until dates must be setted"
+	fi
+
+	git log --since "$1" --until "$2" --no-walk --tags --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cD) %C(bold blue)<%an>%Creset'
+}
+
+glolBetween() {
+	if [ -z "$1" ] && [ -z "$2" ]
+		then
+		echo "Tags between dates: since and until dates must be setted"
+	fi
+
+	git log --since "$1" --until "$2" --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cD) %C(bold blue)<%an>%Creset' --abbrev-commit
+}
+
 alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cD) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias diogenes="git for-each-ref --sort='authoremail' --format='%(committerdate)%09%(authorname) %(authoremail) %(refname)%09=> %(subject)' refs/remotes/origin/ | sed 's/refs\/remotes\/origin\///'"
+alias tagsinfo="git log --no-walk --tags --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cD) %C(bold blue)<%an>%Creset'"
+alias tagsbt=tagsBetween
+alias glolbt=glolBetween
